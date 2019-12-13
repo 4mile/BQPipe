86fcf06931c969553e157bb1cdc5f27e3c254713
Johnathan Brooks <jbrooks93@gmail.com>
2019-12-13 08:14:19 -0800

    Updated init file for new core structure.

--- bqpipe/__init__.py
+++ bqpipe/__init__.py
@@ -1,2 +1,1 @@
-from .core import fetch_table_data, fetch_sql_output, list_datasets, write_to_bigquery, list_tables_in_dataset, \
-    get_table_schema, authenticate_with_service_account_json
+from .core import BigQueryClient
