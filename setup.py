474fc02850960ad2d55daa46c09fb790c06f79f2
Johnathan Brooks <jbrooks93@gmail.com>
2020-04-09 22:57:02 -0700

    Updated version for release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='bqpipe',
-    version='0.5.0',
+    version='0.5.1',
 
     description='Wrapper around BigQuery & Snowflake libraries to simplify writing to/reading from Pandas DataFrames.',
     long_description=long_description,
